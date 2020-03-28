75906b5c3529c5fbe965a17b8c21910fae345afc
Efrain de Leon <efraindeleon12@outlook.com>
2020-03-28 09:19:10 -0600

    first commit

---
+++ Ejercicios.py
@@ -0,0 +1,14 @@
+
+def producto():
+
+    numeroUno= int(input("Introduce el primer numero"))
+    numeroDos= int(input("Introduce el segundo numero"))
+    resultado=0
+    for x in range(0, numeroDos):
+        resultado=resultado + numeroUno
+
+        print(resultado)
+
+
+def producto()
+    while not r:
