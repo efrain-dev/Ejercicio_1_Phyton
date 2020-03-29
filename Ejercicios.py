100c4273061b559a3162be9bb658235672d3ff8a
Efrain de Leon <efraindeleon12@outlook.com>
2020-03-28 21:33:46 -0600

    segundo commint

--- Ejercicios.py
+++ Ejercicios.py
@@ -10,5 +10,33 @@ def producto():
         print(resultado)
 
 
-def producto()
-    while not r:
+def productoraro():
+    num1=0
+    num2=0
+    i = ord('d')
+    while not i == 102:
+
+        num2 = int(input("Ingrese un numero"))
+        i = ord(input("Presione f para salir"))
+        print(num2)
+        num1 = num1 * num2
+    print(num1)
+
+def mayor():
+    num1=0
+    num2=0
+    i = ord('d')
+    while not i == 102:
+
+        num2 = int(input("Ingrese un numero"))
+        i = ord(input("Presione f para salir"))
+        if num2 >= num1:
+            num1=num2
+        print("hola")
+    else:
+        num2=num1
+
+    print("el numero mayor es"+str(num2))
+
+
+mayor()
